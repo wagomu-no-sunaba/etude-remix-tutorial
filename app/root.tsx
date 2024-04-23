17509d51a6acebceb41274132cf93fdbb8f8ab6e
staticWagomU <to6wa17@gmail.com>
2024-04-23 21:25:45 +0900

    update

--- app/root.tsx
+++ app/root.tsx
@@ -5,6 +5,12 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import { LinksFunction } from "@remix-run/node";
+import appStylesHref from "./app.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref },
+]
 
 export default function App() {
   return (
