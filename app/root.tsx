e5f1d233d3faa20c46e85fc22ad5500ea5d44fec
staticWagomU <to6wa17@gmail.com>
2024-11-02 12:06:55 +0900

    feat: 一覧画面作成

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,22 @@
+import { json } from "@remix-run/node";
 import {
   Form,
+  Link,
   Links,
   Meta,
+  Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
 import { LinksFunction } from "@remix-run/node";
 import appStyleHref from "./app.css";
+import { getContacts } from "./data";
+
+export const loader = async () => {
+  const contacts = await getContacts();
+  return json({contacts});
+}
 
 // すべてのrouteでエクスポートできるlink関数
 // <Links />コンポーネントにレンダリングされるらしい
@@ -14,6 +24,7 @@ export const links: LinksFunction = () => [
   { rel: "stylesheet", href: appStyleHref },
 ]
 export default function App() {
+  const { contacts } = useLoaderData<typeof loader>();
   return (
     <html lang="en">
       <head>
@@ -41,17 +52,35 @@ export default function App() {
             </Form>
           </div>
           <nav>
-            <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
-            </ul>
+            {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                  <li key={contact.id}>
+                    <Link to={`contacts/${contact.id}`}>
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>No Name</i>
+                      )}{" "}
+                      {contact.favorite ? (
+                        <span>★</span>
+                      ) : null}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
           </nav>
         </div>
-
+        <div id="detail">
+          <Outlet />
+        </div>
         <ScrollRestoration />
         <Scripts />
       </body>
